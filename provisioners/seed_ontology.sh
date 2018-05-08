866ca4b76fc5e5a3d755f68e3dd2c914870903d8
James Seager <james.seager@rothamsted.ac.uk>
2018-05-08 13:50:59 +0100

    Update seed_ontology provisioner to fix environment variable issue
    Exporting PWD fixed the missing path to the binaries, but the script had to be run with 'sudo', and this doesn't preserve environment variables by default. Adding the '-E' flag (--preserve-env) fixes this.

--- provisioners/seed_ontology.sh
+++ provisioners/seed_ontology.sh
@@ -2,9 +2,13 @@
 
 cd /home/ontology-starter-kit-1.0.2
 
-./seed-my-ontology-repo.pl \
+# Needed to fix a string concatenation error in seed-my-ontology-repo.pl
+export PWD=`pwd`
+
+# The -E flag is needed to preserve environment variables
+sudo -E bash -c './seed-my-ontology-repo.pl \
   -d chebi -d pato -d go \
   -u jseager7 \
   -t "PHIPO" \
   -c \
-  phipo
+  phipo'
