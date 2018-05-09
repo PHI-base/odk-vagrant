5e1383ba50e73ebff82cd5fcd0fea4ab061628c5
James Seager <james.seager@rothamsted.ac.uk>
2018-05-09 13:19:02 +0100

    Make the addition of /bin to the PATH idempotent
    Also involves running seed_ontology.sh in /bin/bash

--- provisioners/seed_ontology.sh
+++ provisioners/seed_ontology.sh
@@ -1,10 +1,12 @@
-!#/usr/bin/sh
+!#/bin/bash
 
 cd /home/ontology-starter-kit-1.0.2
 
-# seed-my-ontology-repo.pl tries (and fails) to do this step itself, so we
-# must run it manually here instead.
-PATH=$PATH:$PWD/bin
+if [[ ":$PATH:" != *":$PWD/bin:"* ]]; then
+  # seed-my-ontology-repo.pl tries (and fails) to do this step itself, so we
+  # must run it manually here instead.
+  PATH=$PATH:$PWD/bin
+fi
 
 sudo ./seed-my-ontology-repo.pl \
   -d chebi -d pato -d go \
