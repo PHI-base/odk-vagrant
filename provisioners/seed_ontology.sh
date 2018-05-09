ed577fd912530fa4bcc03b22cdb958481c1346b0
James Seager <james.seager@rothamsted.ac.uk>
2018-05-09 13:35:43 +0100

    Export the PATH to /bin to the global profile (in seed_ontology.sh)

--- provisioners/seed_ontology.sh
+++ provisioners/seed_ontology.sh
@@ -5,7 +5,9 @@ cd /home/ontology-starter-kit-1.0.2
 if [[ ":$PATH:" != *":$PWD/bin:"* ]]; then
   # seed-my-ontology-repo.pl tries (and fails) to do this step itself, so we
   # must run it manually here instead.
-  PATH=$PATH:$PWD/bin
+  touch /etc/profile.d/osk.sh \
+  && echo "PATH=\$PATH:\$PWD/bin" > /etc/profile.d/osk.sh
+  source ~/.profile
 fi
 
 sudo ./seed-my-ontology-repo.pl \
