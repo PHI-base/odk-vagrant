79068cccda5e5d627cdf4f688584aa96635df644
James Seager <james.seager@rothamsted.ac.uk>
2018-05-10 14:09:50 +0100

    Use bugfix fork of OSK for downloading starter pack

--- provisioners/get_starter_pack.sh
+++ provisioners/get_starter_pack.sh
@@ -5,7 +5,7 @@ cd /home/
 # Note that you MUST have wget version 1.16 installed for this to work!
 # Earlier versions of wget do not support TLS v1.2
 wget --secure-protocol=TLSv1_2 \
-  https://github.com/INCATools/ontology-starter-kit/archive/v1.0.2.tar.gz
+  https://github.com/kimrutherford/ontology-starter-kit/archive/master.tar.gz
 
-tar -xzvf v1.0.2.tar.gz
-rm v1.0.2.tar.gz
+tar -xzvf master.tar.gz
+rm master.tar.gz
