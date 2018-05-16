44fcd5d94b77e80bc5e50b478d1695e255bf2260
James Seager <james.seager@rothamsted.ac.uk>
2018-05-16 13:40:39 +0100

    Add some explanation to seed_ontology.sh

--- provisioners/seed_ontology.sh
+++ provisioners/seed_ontology.sh
@@ -2,6 +2,7 @@
 
 cd /home/ontology-starter-kit-*
 
+# We have to add the binaries to the path, since the script fails to do it
 sudo PATH=$PATH:$PWD/bin ./seed-my-ontology-repo.pl \
   -d pato \
   -u jseager7 \
