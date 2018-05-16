d6514b1969a1e1ab985b8932d585f8812eb62037
James Seager <james.seager@rothamsted.ac.uk>
2018-05-16 13:39:52 +0100

    Automatically change user name and email for seed commits

--- provisioners/set_git_user.sh
+++ provisioners/set_git_user.sh
@@ -4,3 +4,12 @@ cd /home/ontology-starter-kit-*/target/phipo
 
 git config user.name "$0"
 git config user.email "$1"
+
+# Rewrite the existing commits to use the correct author
+git filter-branch --commit-filter '
+  GIT_AUTHOR_NAME="$0";
+  GIT_COMMITTER_NAME="$0";
+  GIT_AUTHOR_EMAIL="$1";
+  GIT_COMMITTER_EMAIL="$1";
+  git commit-tree "$@";
+' HEAD
