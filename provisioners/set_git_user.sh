01f0b1eb7bcb2b6493c6999296bc9439a47f1db3
James Seager <james.seager@rothamsted.ac.uk>
2018-05-16 13:35:58 +0100

    Revert "Update git user provisioner to set global config"
    This reverts commit e57bcec2e1cbbe19fe271d47729170ae5ffdb5fa.
    
    The seed script doesn't read the global config, presumably because the script is run as root.

--- provisioners/set_git_user.sh
+++ provisioners/set_git_user.sh
@@ -1,14 +1,6 @@
 #!/bin/sh
 
-# Unfortunately we have to set the global git config, since until we run the
-# ontology seed script, no git repository exists for us to set the local 
-# configuration, and we can't set git user details during the script.
+cd /home/ontology-starter-kit-*
 
-# Not using '~', since the provisioner runs as root
-cd /home/vagrant
-
-# Have to create .git in the home folder, since it doesn't initially exist
-mkdir -p .git
-
-git config --global user.name "$0"
-git config --global user.email "$1"
+git config user.name "$0"
+git config user.email "$1"
