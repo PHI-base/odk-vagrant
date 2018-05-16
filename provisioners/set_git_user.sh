e57bcec2e1cbbe19fe271d47729170ae5ffdb5fa
James Seager <james.seager@rothamsted.ac.uk>
2018-05-16 12:19:47 +0100

    Update git user provisioner to set global config

--- provisioners/set_git_user.sh
+++ provisioners/set_git_user.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 
-cd /home/ontology-starter-kit-*
+# Unfortunately we have to set the global git config, since until we run the
+# ontology seed script, no git repository exists for us to set the local 
+# configuration, and we can't set git user details during the script.
 
-git config user.name "$0"
-git config user.email "$1"
+# Not using '~', since the provisioner runs as root
+cd /home/vagrant
+
+# Have to create .git in the home folder, since it doesn't initially exist
+mkdir -p .git
+
+git config --global user.name "$0"
+git config --global user.email "$1"
