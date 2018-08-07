5698456a21dcfb663f8b504c14eb7b0d53ba1fc1
James Seager <james.seager@rothamsted.ac.uk>
2018-08-07 11:59:43 +0100

    Make a comment less stroppy

--- provisioners/get_starter_pack.sh
+++ provisioners/get_starter_pack.sh
@@ -2,8 +2,8 @@
 
 cd /home/
 
-# Note that you MUST have wget version 1.16 installed for this to work!
-# Earlier versions of wget do not support TLS v1.2
+# Note that you must have wget version 1.16 or later installed:
+# earlier versions of wget do not support TLS v1.2
 wget --secure-protocol=TLSv1_2 \
   https://github.com/INCATools/ontology-development-kit/archive/v1.1.3.tar.gz
 
