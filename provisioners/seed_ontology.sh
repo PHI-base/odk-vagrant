78421625dda12575d6e554534c9697c688af0735
James Seager <james.seager@rothamsted.ac.uk>
2018-08-07 13:01:03 +0100

    Change GitHub user to 'PHI-base'

--- provisioners/seed_ontology.sh
+++ provisioners/seed_ontology.sh
@@ -5,7 +5,7 @@ cd /home/ontology-starter-kit-*
 # We have to add the binaries to the path, since the script fails to do it
 sudo PATH=$PATH:$PWD/bin ./seed-my-ontology-repo.pl \
   -d pato \
-  -u jseager7 \
+  -u PHI-base \
   -t "PHIPO" \
   -c \
   phipo
