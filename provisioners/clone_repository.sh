8ef597e9edb08c28dcc484845eb4043ef3a0fc1e
James Seager <james.seager@rothamsted.ac.uk>
2018-09-20 12:26:10 +0100

    Fix permissions issues with ontology repository

--- provisioners/clone_repository.sh
+++ provisioners/clone_repository.sh
@@ -5,3 +5,11 @@ REPO_URL=$1
 cd /home/vagrant/
 
 git clone ${REPO_URL}
+
+# Transfer ownership to the default vagrant user, since commands may need 
+# to be run manually from within the VM later.
+chown -R vagrant:vagrant phipo
+
+# Ensure all the script files in the ontology directory are executable.
+cd /phipo/src/ontology
+chmod +x *.sh
